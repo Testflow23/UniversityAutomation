9fc70273fdd45249c540aa272c82202d3ed52667
Testflow23 <123447794+Testflow23@users.noreply.github.com>
2023-05-02 04:57:07 -0400

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using UniversityManagement.Models;
+
+namespace UniversityManagement
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConString")));
+        }    }
+}
